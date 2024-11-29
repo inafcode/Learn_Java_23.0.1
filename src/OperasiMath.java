c2f34fce85bce86df20fb9ffcec669f2bb126245
IlhamNaffi-12210621 <naffiilham@gmail.com>
2024-11-29 14:11:22 +0700

    Math, Boolean, Banding

---
+++ src/OperasiMath.java
@@ -0,0 +1,35 @@
+public class OperasiMath {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+
+        int c = 100;
+
+        c +=10;
+        System.out.println(c);
+
+        c -=10;
+        System.out.println(c);
+
+        c *=10;
+        System.out.println(c);
+
+        int d = +100;
+        int e = -10;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+
+        System.out.println(!true);
+    }
+}
